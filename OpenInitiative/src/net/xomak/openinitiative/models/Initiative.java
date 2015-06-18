c83ee19d5709016a9dd1dea515b7a0489e3d1fe5
Xomak <mail@xomak.net>
2015-06-18 21:59:18 +0300

    First test commit

---
+++ OpenInitiative/src/net/xomak/openinitiative/models/Initiative.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package net.xomak.openinitiative.models;
+
+/**
+ * Model of initiative
+ * @author Konstantin Danilov
+ */
+public class Initiative {
+
+	protected int id;
+	protected String name;
+	protected String description;
+	protected String text;
+	protected Status status;
+	/**
+	 * 
+	 */
+	public Initiative() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ OpenInitiative/src/net/xomak/openinitiative/models/Status.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package net.xomak.openinitiative.models;
+
+/**
+ * @author Konstantin Danilov
+ *
+ */
+public class Status {
+
+	/**
+	 * 
+	 */
+	public Status() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
